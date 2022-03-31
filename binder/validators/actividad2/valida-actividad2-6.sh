5f2f65394e5f5b228c89940c3ff931ea7c075a36
alejokalderon <alejokalderon@gmail.com>
2022-03-31 17:08:50 -0500

    Add files via upload

---
+++ binder/validators/actividad2/valida-actividad2-6.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+RESPUESTA=$(cat /tmp/respuesta2-6.txt 2>/dev/null)
+  if [ -z "$RESPUESTA" ]
+  then
+    echo "Introduce un valor e intenta de nuevo."
+    echo ""
+  else
+  if [ $RESPUESTA == "RESPUESTA" ]
+  then
+    echo "No olvides cambiar el texto 'RESPUESTA' por el número que consideres"
+  else
+  if [ $RESPUESTA -eq "2" ]
+  then
+    echo "Correcto:"
+    echo ""
+    echo "Se imprimirían '2' id diferentes, el del usuario 'usuario1' y el del usuario 'usuario2'"
+    echo ""
+    echo "Observemos línea a línea la explicación:"
+    echo ""
+    echo "1. whoami: Imprime quienes somos actualmente"
+    echo "usuario1: La respuesta de 'whoami' es 'usuario1'"
+    echo ""
+    echo "2. id: Imprime el id del usuario"
+    echo "1001: La respuesta de 'id', el cual es el id del usuario 'usuario1' '1001'"
+    echo ""
+    echo "3. su usuario2: Nos convertimos en el usuario 'usuario2'"
+    echo ""
+    echo "4. id: Imprime el id del usuario"
+    echo "1002: La respuesta de 'id', el cual es el id del usuario 'usuario2'"
+  else
+    echo "Fallaste, revisa bien los comandos y cambia el número por la respuesta correcta."
+  fi
+  fi
+  fi
+
---
+++ binder/validators/actividad2/valida-actividad3-6.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+RESPUESTA=$(cat /tmp/respuesta3-6.txt 2>/dev/null)
+  if [ -z "$RESPUESTA" ]
+  then
+    echo "Introduce un valor e intenta de nuevo."
+    echo ""
+  else
+  if [ $RESPUESTA == "RESPUESTA" ]
+  then
+    echo "No olvides cambiar el texto 'RESPUESTA' por el número que consideres"
+  else
+  if [ $RESPUESTA -eq "3" ]
+  then
+    echo "Correcto:"
+    echo ""
+    echo "Se imprimirían '3' directorios personales diferentes, el del usuario '/home/usuario1', el del usuario '/home/usuario2' y el del usuario root '/root'"
+    echo ""
+    echo "Observemos línea a línea la explicación:"
+    echo ""
+    echo "1. whoami: Imprime quienes somos actualmente"
+    echo "usuario1: La respuesta de 'whoami' es 'usuario1'"
+    echo ""
+    echo "2. echo ~"
+    echo "/home/usuario1: La respuesta de 'echo ~', muestra el directorio del usuario 'usuario1'"
+    echo ""
+    echo "3. su usuario2: Nos convertimos en el usuario 'usuario2'"
+    echo ""
+    echo "4. echo ~"
+    echo "/home/usuario2: La respuesta de 'echo ~', muestra el directorio del usuario 'usuario2'"
+    echo ""
+    echo "5. su root: Nos convertimos en el usuario 'root'"
+    echo ""
+    echo "6. echo ~"
+    echo "/root: La respuesta de 'echo ~', muestra el directorio del usuario 'root'"
+  else
+    echo "Fallaste, revisa bien los comandos y cambia el número por la respuesta correcta."
+  fi
+  fi
+  fi
+
